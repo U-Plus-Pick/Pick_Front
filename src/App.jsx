be8b3988c4c2464857d6d917d03ceb505423c17b
박용규 <yoyo5411@naver.com>
2025-06-19 16:08:46 +0900

    feat : HeroBundle LayOut 완

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import HeroChatBot from './components/MainSection/HeroChatBot'
 import HeroSection from './components/MainSection/HeroSection'
 import HeroSlogun from './components/MainSection/HeroSlogun'
 import HeroUPI from './components/MainSection/HeroUPI'
+import HeroBundle from './components/MainSection/HeroBundle'
 
 function App() {
   return (
     <div>
-      <HeroSection />
-      <HeroSlogun />
-      <HeroUPI />
-      <HeroChatBot />
+      <HeroBundle />
     </div>
   )
 }
