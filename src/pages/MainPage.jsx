6ca3f06651ff5729e801eb8ad3fd54727dd514bc
박용규 <yoyo5411@naver.com>
2025-06-20 11:37:59 +0900

    feat : Header ScrollDown/Up 기능

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useScrollDirection } from 'react-use-scroll-direction'
+
 import HeroChatBot from '../components/MainSection/HeroChatBot'
 import HeroSection from '../components/MainSection/HeroSection'
 import HeroSlogun from '../components/MainSection/HeroSlogun'
@@ -9,6 +11,21 @@ import HeroRelation from '../components/MainSection/HeroRelation'
 import HeroLastPage from '../components/MainSection/HeroLastPage'
 
 function MainPage() {
+  const { isScrollingUp, isScrollingDown } = useScrollDirection()
+
+  useEffect(() => {
+    const header = document.querySelector('.header')
+    if (!header) return
+
+    if (isScrollingDown) {
+      header.classList.add('hide-header')
+      header.classList.remove('show-header')
+    } else if (isScrollingUp) {
+      header.classList.add('show-header')
+      header.classList.remove('hide-header')
+    }
+  }, [isScrollingUp, isScrollingDown])
+
   return (
     <div>
       <HeroSection />
