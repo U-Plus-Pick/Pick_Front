2e961edcf22b887b9f95c62479af1ebd6cb7c700
박용규 <yoyo5411@naver.com>
2025-06-21 19:22:16 +0900

    feat : Header Scrolle 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useScrollDirection } from 'react-use-scroll-direction'
+import React, { useEffect, useRef } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import HeroChatBot from '../components/MainSection/HeroChatBot'
 import HeroSection from '../components/MainSection/HeroSection'
@@ -11,20 +11,37 @@ import HeroRelation from '../components/MainSection/HeroRelation'
 import HeroLastPage from '../components/MainSection/HeroLastPage'
 
 function MainPage() {
-  const { isScrollingUp, isScrollingDown } = useScrollDirection()
+  const location = useLocation()
+  const prevScroll = useRef(0)
 
   useEffect(() => {
     const header = document.querySelector('.header')
-    if (!header) return
+    if (!header || location.pathname !== '/') return
 
-    if (isScrollingDown) {
-      header.classList.add('hide-header')
-      header.classList.remove('show-header')
-    } else if (isScrollingUp) {
-      header.classList.add('show-header')
-      header.classList.remove('hide-header')
+    const handleScroll = () => {
+      const currentScroll = window.scrollY
+
+      if (currentScroll > prevScroll.current) {
+        // 내려갔을 때
+        header.classList.add('hide-header')
+        header.classList.remove('show-header')
+      } else if (currentScroll < prevScroll.current) {
+        // 올라갔을 때
+        header.classList.add('show-header')
+        header.classList.remove('hide-header')
+      }
+
+      prevScroll.current = currentScroll
     }
-  }, [isScrollingUp, isScrollingDown])
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+      header.classList.remove('hide-header')
+      header.classList.add('show-header')
+    }
+  }, [location.pathname])
 
   return (
     <div>
