7bdcaed3fb5e370376a2d900e975b43bec1901f2
박용규 <yoyo5411@naver.com>
2025-06-23 11:27:09 +0900

    feat : BundleRole 수정 완료

--- src/components/Bundle/BundleRoleCard.jsx
+++ src/components/Bundle/BundleRoleCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import css from '../../styles/scss/BundleRole.module.scss'
-import BundleIntro1 from '../../assets/BundleIntro1.png'
 
-export default function BundleRoleCard({ title, desc, highlight, activeTab }) {
+export default function BundleRoleCard({ title, desc, highlight, img, type }) {
   return (
-    <div className={`${css.card} ${css[activeTab]}`}>
-      <h3>{title}</h3>
-      <img src={BundleIntro1} alt="혜택 이미지" />
+    <div className={`${css.card} ${css[type]}`}>
+      <h3 className={css.title}>{title}</h3> {/* h3에 따로 class 분기 필요 없음 */}
+      <img src={img} alt="혜택 이미지" />
       <p>
         {desc}
         {highlight && <span className={css.highlight}>({highlight})</span>}
