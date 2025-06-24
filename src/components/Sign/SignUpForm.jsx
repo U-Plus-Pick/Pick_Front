903479842849a9fd3c26ecfa51be1e95d141962d
박용규 <yoyo5411@naver.com>
2025-06-24 20:18:45 +0900

    feat : SignUpForm 변수 수정

--- src/components/Sign/SignUpForm.jsx
+++ src/components/Sign/SignUpForm.jsx
@@ -109,7 +109,7 @@ const SignUpForm = ({ goToLogin }) => {
         phone: form.phone.replace(/-/g, ''),
         password: form.password,
         passwordConfirm: form.confirmPassword,
-        plan_name: form.plan,
+        plan: form.plan,
       })
 
       if (response.data?.message === '회원가입 성공') {
